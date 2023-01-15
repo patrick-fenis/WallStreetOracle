84b87b52ef85cc4dd6b11a094dcbd858e280120c
andriy.mereshchak@uconn.edu.com <andriy.mereshchak@uconn.edu>
2023-01-15 18:59:20 -0500

    API still works OK

--- js/domApi.js
+++ js/domApi.js
@@ -1,9 +1,17 @@
-fetch ('https://api.stockdata.org/v1/data/quote?symbols=LMT&api_token=E8gohHXE0bQzggOAYqRNwzZigNrJVbQxUlS4soLq').then
+window.onload = (event) => {
+document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const search = document.querySelector('input[type="text"]').value
+})
+    fetch ('https://api.stockdata.org/v1/data/quote?symbols=LMT&api_token=E8gohHXE0bQzggOAYqRNwzZigNrJVbQxUlS4soLq').then
 ((response) => {
     return response.json()
 }).then((json) => {
     console.log(json)
+    document.querySelector('#symbol').innerText = json.data[0].ticker
+    document.querySelector('#company-name').innerText = json.data[0].name
 },
 (err) => {
     console.log(err)
-}) 
+})
+} 
