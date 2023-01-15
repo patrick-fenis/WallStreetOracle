6e176bd160ed822b87f470642113fa833ac3efdb
andriy.mereshchak@uconn.edu.com <andriy.mereshchak@uconn.edu>
2023-01-15 18:26:49 -0500

    API connected

--- js/domApi.js
+++ js/domApi.js
@@ -1,1 +1,9 @@
-fetch ('https://api.stockdata.org/v1/data/quote?symbols=LMT&api_token=E8gohHXE0bQzggOAYqRNwzZigNrJVbQxUlS4soLq')
+fetch ('https://api.stockdata.org/v1/data/quote?symbols=LMT&api_token=E8gohHXE0bQzggOAYqRNwzZigNrJVbQxUlS4soLq').then
+((response) => {
+    return response.json()
+}).then((json) => {
+    console.log(json)
+},
+(err) => {
+    console.log(err)
+}) 
