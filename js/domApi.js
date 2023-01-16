135700a76b8827405d698276959cfd111a7da98d
andriy.mereshchak@uconn.edu.com <andriy.mereshchak@uconn.edu>
2023-01-15 19:17:57 -0500

    API takes Search Input

--- js/domApi.js
+++ js/domApi.js
@@ -2,16 +2,18 @@ window.onload = (event) => {
 document.querySelector('form').addEventListener('submit', (e) => {
     e.preventDefault()
     const search = document.querySelector('input[type="text"]').value
-})
-    fetch ('https://api.stockdata.org/v1/data/quote?symbols=LMT&api_token=E8gohHXE0bQzggOAYqRNwzZigNrJVbQxUlS4soLq').then
-((response) => {
+    fetch (`https://api.stockdata.org/v1/data/quote?symbols=${search}&api_token=E8gohHXE0bQzggOAYqRNwzZigNrJVbQxUlS4soLq`).then
+    ((response) => {
     return response.json()
-}).then((json) => {
-    console.log(json)
-    document.querySelector('#symbol').innerText = json.data[0].ticker
-    document.querySelector('#company-name').innerText = json.data[0].name
-},
-(err) => {
-    console.log(err)
+    }).then((json) => {
+        console.log(json)
+        document.querySelector('#symbol').innerText = json.data[0].ticker
+        document.querySelector('#company-name').innerText = json.data[0].name
+    },
+    (err) => {
+        console.log(err)
+        document.querySelector('#symbol').innerText = 'Try Again'
+        document.querySelector('#company-name').innerText = 'Not a valid stock ticker. ex. IONQ'
+})
 })
 } 
